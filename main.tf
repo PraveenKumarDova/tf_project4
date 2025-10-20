4f8dae0a0df3e53beb8df16e8f520b14a97e25b4
Praveen Kumar Dova <praveenkumardova@gmail.com>
2025-10-20 01:00:51 +0100

    tf files

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.17.0"
     }
   }
@@ -10,4 +10,47 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "eu-west-2"
+}
+
+variable "instance_type" {
+
+}
+
+data "aws_ami" "myami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-kernel-5.10-hvm-2.0.20250801-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "webserver" {
+  # count         = 2
+  ami           = data.aws_ami.myami.id
+  instance_type = var.instance_type
+
+  tags = {
+    # Name = "webserver-${count.index + 1}"
+    Name = "webserver"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.webserver.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.webserver.id
+}
+
+output "ami" {
+  value = aws_instance.webserver.ami
 }
